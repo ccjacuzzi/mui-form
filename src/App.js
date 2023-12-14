c6dbb44af7bd7d29fe88c8d12e7bf4f0e5d95ac6
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-12-14 11:25:59 -0600

    Learning some tips on building forms with React and MUI.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { AppBar, Toolbar} from '@mui/material';
+import { Container } from '@mui/system';
+import React from 'react';
+ 
+import { Route, Routes } from "react-router-dom"
+import Login from './components/Login';
+import RegisterForm from './components/Register';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <AppBar position='relative'><Toolbar>MUI Form</Toolbar></AppBar>
+    <Container>
+    <Routes>
+      <Route path="/" element={<RegisterForm />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+   
+     
+    </Container>
+    </React.Fragment>
   );
 }
-
+ 
 export default App;
